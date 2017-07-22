c63f2a6580680d2ad5aeb2a9be97b1137c0c43c5
Jan Stabenow <hello@jan-stabenow.de>
2017-07-22 23:48:44 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,15 +8,9 @@ for VERSION in $FFMPEG_VERSIONS
 do
 
     # build amd64 image
-    docker build --build-arg FFMPEG_VERSION=$VERSION -f Dockerfile_AMD64 -t datarhei/ffmpeg:$VERSION .
-    docker push datarhei/ffmpeg:$VERSION
+    docker build --build-arg FFMPEG_VERSION=$VERSION -f Dockerfile_AMD64 -t ffmpeg:$VERSION .
     
     # build armhf image
-    docker build --build-arg FFMPEG_VERSION=$VERSION -f Dockerfile_ARMHF -t datarhei/ffmpeg:$VERSION-armhf .
-    docker push datarhei/ffmpeg:$VERSION-armhf
-    
-    # build armhf omx image
-    docker build --build-arg FFMPEG_VERSION=$VERSION -f Dockerfile_ARMHF_OMX -t datarhei/ffmpeg:$VERSION-armhf .
-    docker push datarhei/ffmpeg:$VERSION-armhf-omx
+    docker build --build-arg FFMPEG_VERSION=$VERSION -f Dockerfile_ARMHF -t ffmpeg:$VERSION-armhf .
     
 done
