ee348856312bfc4bbec1f63affd5ef03170e6022
Jan Stabenow <hello@jan-stabenow.de>
2017-07-23 00:02:48 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,9 +7,6 @@ FFMPEG_VERSIONS=$@
 for VERSION in $FFMPEG_VERSIONS
 do
 
-    # build amd64 image
-    docker build --build-arg FFMPEG_VERSION=$VERSION -f Dockerfile_AMD64 -t ffmpeg:$VERSION .
-    
     # build armhf image
     docker build --build-arg FFMPEG_VERSION=$VERSION -f Dockerfile_ARMHF -t ffmpeg:$VERSION-armhf .
     
